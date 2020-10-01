3b94ba770a31b1fd1f647585e9e108d460581d9b
LazyGon <lazygon.mc@gmail.com>
2020-10-01 14:49:55 +0900

    [Change] WIthdrawStickGUI potion throwing setting icon

--- src/main/java/net/okocraft/boxstick/gui/WithdrawStickGUI.java
+++ src/main/java/net/okocraft/boxstick/gui/WithdrawStickGUI.java
@@ -18,7 +18,7 @@ public class WithdrawStickGUI extends StickSettingGUI {
 
         buttonList.putButton(1, 0, createBoolSettingButton(data.getOnPlaceBlock(), Material.BRICKS, stick));
         buttonList.putButton(1, 2, createBoolSettingButton(data.getOnEatFood(), Material.COOKED_BEEF, stick));
-        buttonList.putButton(1, 4, createBoolSettingButton(data.getOnThrowPotion(), Material.POTION, stick));
+        buttonList.putButton(1, 4, createBoolSettingButton(data.getOnThrowPotion(), Material.SPLASH_POTION, stick));
         buttonList.putButton(1, 6, createBoolSettingButton(data.getOnBreakItem(), Material.IRON_PICKAXE, stick));
         buttonList.putButton(1, 8, otherStickButton);
 
